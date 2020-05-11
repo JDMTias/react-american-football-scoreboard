f2270cb3977175680534526cd2ce581cc126164c
JDMTias <matiasiturbide@gmail.com>
2020-05-11 17:14:36 -0600

    Forgot to commit today, I know I need to commit after everystep

---
+++ src/home.js
@@ -0,0 +1,45 @@
+// import React, {useState} from "react";
+// import "./App.css";
+
+
+// function homeData() {
+//     //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+//   const [homeScore, setHomeScore] = useState(0)
+  
+  
+//   function  homeTouchdown () {
+//     setHomeScore(homeScore + 7);
+//   }
+  
+//   function homeFieldgoal () {
+//     setHomeScore(homeScore + 3);
+//   }
+  
+//     return (
+//       <div className="container">
+//         <section className="scoreboard">
+//           <div className="topRow">
+//             <div className="home">
+//               <h2 className="home__name">Lions</h2>
+  
+//               {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+//               <div className="home__score">{homeScore}</div>
+//             </div>
+//         </section>
+//         <section className="buttons">
+//           <div className="homeButtons">
+  
+//             {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+//             <button className="homeButtons__touchdown" onClick={homeTouchdown}>Home Touchdown</button>
+//             <button className="homeButtons__fieldGoal" onClick={homeFieldgoal}>Home Field Goal</button>
+//           </div>
+//           <div className="awayButtons">
+//             <button className="awayButtons__touchdown" onClick={awayTouchdown}>Away Touchdown</button>
+//             <button className="awayButtons__fieldGoal" onClick={awayFieldgoal}>Away Field Goal</button>
+//           </div>
+//         </section>
+//       </div>
+//     );
+//   }
+  
+//   export default App;
